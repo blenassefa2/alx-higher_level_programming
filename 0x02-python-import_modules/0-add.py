0c22d1c9126e356418446f9668fec04c5ce05b5a
blenassefa2 <blenassefa59@yahoo.com>
2022-06-08 21:03:18 +0300

    organized a few things

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
+
+
 def my_func():
     a = 1
     b = 2
-    print(f"{a:d} + {b:d} = {add(a, b}:d")
+    print(f"{a:d} + {b:d} = {add(a, b):d}")
+
 
 if __name__ == '__main__':
     my_func()
