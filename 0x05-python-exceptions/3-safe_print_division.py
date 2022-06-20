9108a0c1c88c31c82c651173b9fc4887bcd0be11
blenassefa2 <blenassefa59@yahoo.com>
2022-06-20 23:10:50 +0300

    python error fourth task

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    ans = None
+    try:
+        ans = a / b
+    except Exception as exception:
+        pass
+    finally:
+        print("Inside result: {}".format(ans))
+        return ans
+        
