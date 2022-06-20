dde83a12df55d27b2a939fc575054fa05d6e11d2
blenassefa2 <blenassefa59@yahoo.com>
2022-06-20 23:32:00 +0300

    fix errors

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except(ZeroDivisionError):
         pass
     finally:
-        print("Inside result: {}".format(ans))
+        print("{}".format(ans))
         return ans 
