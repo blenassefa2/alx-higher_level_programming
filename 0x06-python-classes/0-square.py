b16f770cf089088b2bf892769004608eea286cf2
blenassefa2 <blenassefa59@yahoo.com>
2022-06-21 17:53:14 +0300

    python class task

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """
+
+    This is an empty class and no description is needed
+
+    """
+    def __init__(self):
+        """
+        Note:
+            this is an empty init
+        """
+        pass
