4bf34988b2ee587c5fc569754a678b88493686ba
blenassefa2 <blenassefa59@yahoo.com>
2022-06-22 23:31:27 +0300

    fix errors

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This is a class for a square with attribute size"""
+
+
+class Square:
+    """This is a square class """
+
+    def __init__(self, size=0):
+        """
+        Constructs all the necessary attributes for square
+
+        Parameters
+        ----------
+        size : int
+            the size of square object
+        """
+        try:
+            self.__size = size
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        except Exception as exception:
+            raise exception
+
+    @property
+    def size(self):
+        """
+        gets the current sqare size
+
+        Returns:
+            The size of the current object
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        setter method for size
+
+        Args:
+            value : int
+            the size of square object
+        """
+        try:
+            self.__size = value
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+        except Exception as exception:
+            raise exception
+
+    def area(self):
+        """
+        Calculates the current sqare area
+
+        Returns:
+            The area of the current object
+        """
+        return self.__size * self.__size
