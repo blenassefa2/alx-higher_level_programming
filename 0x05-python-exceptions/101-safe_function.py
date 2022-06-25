20150a3d63012bbff0799669c1725b0db138fa18
blenassefa2 <blenassefa59@yahoo.com>
2022-06-25 06:38:40 +0300

    started doing optional

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        a = fct(*args)
+        return a
+    except Exception as err:
+        print(f"Exception: {err}", file=sys.stderr)
+        return None
