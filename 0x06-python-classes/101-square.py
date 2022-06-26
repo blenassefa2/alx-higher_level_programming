9699a2fa92c98bc9d78ebf767491aabcaa2447aa
blenassefa2 <blenassefa59@yahoo.com>
2022-06-26 07:26:03 +0300

    fixed err

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This is a class for a square with attribute size"""
+
+
+class Square:
+    """This is a square class """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Constructs all the necessary attributes for square
+
+        Parameters
+        ----------
+        size : int
+            the size of square object
+        position: tuple
+            positions of lines
+        """
+        try:
+            self.__position = position
+            self.__size = size
+            err = "position must be a tuple of 2 positive integers"
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            if len(position) != 2:
+                raise TypeError(err)
+            if not isinstance(position[0], int):
+                raise TypeError(err)
+            if not isinstance(position[1], int):
+                raise TypeError(err)
+            if position[0] < 0 or position[1] < 0:
+                raise TypeError(err)
+        except Exception as exception:
+            raise exception
+
+    @property
+    def size(self):
+        """
+        gets the current sqare size
+
+        Returns:
+            The size of the current object
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        setter method for size
+
+        Args:
+            value : int
+            the size of square object
+        """
+        try:
+            self.__size = value
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+        except Exception as exception:
+            raise exception
+
+    @property
+    def position(self):
+        """
+        gets the current square positions
+
+        Returns:
+            The positions of the current object
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        setter method for position
+
+        Args:
+            value : tuple
+            the position of square object
+        """
+        err = "position must be a tuple of 2 positive integers"
+        try:
+            self.__position = value
+
+            if len(position) != 2:
+                raise TypeError(err)
+            if not isinstance(position[0], int):
+                raise TypeError(err)
+            if not isinstance(position[1], int):
+                raise TypeError(err)
+            if position[0] < 0 or position[1] < 0:
+                raise TypeError(err)
+        except Exception as exception:
+            raise exception
+
+    def area(self):
+        """
+        Calculates the current sqare area
+
+        Returns:
+            The area of the current object
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints the current sqare area """
+        s = ""
+        if self.__size == 0:
+            return s
+        a, b = self.__position
+        s = s + "\n"*b
+        for i in range(self.__size):
+            for y in range(a):
+                s += " "
+            for j in range(self.__size):
+                s += "#"
+            s += "\n"
+        print(s[:-1], end="")
+
+    def __str__(self):
+        """ method that makes Sqare object printable"""
+        self.my_print()
+        return ""
