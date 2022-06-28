5a9e46ab0ae624eb52b823a088627224d1cb58c5
blenassefa2 <blenassefa59@yahoo.com>
2022-06-28 11:06:22 +0300

    added class method

--- 0x08-python-more_classes/0-main.py
+++ 0x08-python-more_classes/0-main.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-Rectangle = __import__('8-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
-myrectangle1 = Rectangle(8, 4) 
-myrectangle2 = Rectangle(1, 8)
-
-try: 
-    print(myrectangle2 == Rectangle.bigger_or_equal('Rect', myrectangle1)) 
-except Exception as e: 
-    print("[{}] {}".format(e.__class__.__name__, e))
+my_square = Rectangle.square(5)
+print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
+print(my_square)
