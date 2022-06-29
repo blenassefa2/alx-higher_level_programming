2aea5c68cf5163d5111cf403b903ac4292034335
blenassefa2 <blenassefa59@yahoo.com>
2022-06-29 22:29:46 +0300

    fixed err

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -35,7 +35,7 @@ def calcpos(N):
         Return:
             nothing
         """
-        if j == N:
+        if j >= N:
             print(ans)
             return
         for c in range(N):
@@ -59,14 +59,14 @@ def calcpos(N):
 
 
 if len(sys.argv) != 2:
-    print("Usage: nqueens N", file=sys.stderr)
+    print('Usage: nqueens N')
     exit(1)
 
 if not sys.argv[1].isdigit():
-    print("N must be a number", file=sys.stderr)
+    print('N must be a number')
     exit(1)
 n = int(sys.argv[1])
 if n < 4:
-    print("N must be at least 4", file=sys.stderr)
+    print('N must be at least 4')
     exit(1)
 a = calcpos(n)
