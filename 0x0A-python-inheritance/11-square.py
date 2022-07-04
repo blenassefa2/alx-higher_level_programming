837fd8501cf8b399959ccf2d8598170ee3c79ef6
blenassefa2 <blenassefa59@yahoo.com>
2022-07-05 00:51:08 +0300

    inheritance task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" this module creates a class that inherits from rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square class that inherits rectangle"""
+
+    def __init__(self, size):
+        """
+        initializes the objects appropriatly
+
+        Parameter
+        ---------
+            size : int
+                size of square instance
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """implementation of method inherited from rectangle"""
+
+        return self.__size * self.__size
+
+    def __str__(self):
+        """makes the class printable"""
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
