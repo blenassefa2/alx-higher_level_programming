ea9997cb7a044ad2b562be8ec2e7622f10321ed7
blenassefa2 <blenassefa59@yahoo.com>
2022-07-05 09:18:08 +0300

    fixed errors

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,36 +1,7 @@
 #!/usr/bin/python3
 """module that defines Rectangle"""
 
-
-class BaseGeometry:
-    """a class with unimplemented method"""
-
-    def area(self):
-        """a public instance method
-        Raises:
-            Exception - area() is not implemented
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        A public instance method
-
-        Args:
-            name : string
-                name of variable
-            value : int
-                integer to be validated
-
-        Raises:
-            TypeError - <name> must be an integer
-            ValueError - <name> must be greater than 0
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """class that inherits BaseGeometry class"""
--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-add_attribute = __import__('101-add_attribute').add_attribute
+Rectangle = __import__('8-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class MyClass():
-    pass
-
-mc = MyClass()
-add_attribute(mc, "name", "John")
-print(mc.name)
-
-try:
-    a = "My String"
-    add_attribute(a, "name", "Bob")
-    print(a.name)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+print(issubclass(Rectangle, BaseGeometry))
