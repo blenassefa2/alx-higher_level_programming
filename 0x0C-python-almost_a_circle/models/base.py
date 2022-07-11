f4c426adf266bed71a6bcdc2dec3d8750cf11cb2
blenassefa2 <blenassefa59@yahoo.com>
2022-07-11 22:29:21 +0300

    created base class with one public instance attr and one private class attr

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/bin/usr/python3
+"""a module that defines a class called base"""
+
+
+class Base:
+    """A class called base"""
+
+    __nb_objects = 0  # Private class attr
+
+    def __init__(self, id=None):
+        """initializes a Base object appropriately
+
+        Parameter
+        ---------
+            id : int
+                integer value used later
+        """
+        self.id = None  # Public instance attr
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
