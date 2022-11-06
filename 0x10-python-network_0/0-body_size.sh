be9f2bd76dd8dd83ef8443409c3e696369ce2d30
blenassefa2 <blenassefa59@yahoo.com>
2022-11-06 15:47:35 +0300

    extracted content-length from curl response header

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Extracts the content length of http response
-
-var="$(curl -I -s $1 | grep ^Content-Length: | cut -d: -f2- | sed 's/^ *\(.*\).*/\1/')"
-echo "$var"
+echo "$(curl -I -s $1 | grep ^Content-Length: | cut -d: -f2- | sed 's/^ *\(.*\).*/\1/')"
