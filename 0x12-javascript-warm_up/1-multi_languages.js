d74f7486b606d837f80e5af7710b2bbff89593cb
blenassefa2 <blenassefa59@yahoo.com>
2022-11-18 05:34:54 +0300

    javascript warming up2

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const sentence1 = 'C is fun';
+const sentence2 = 'Python is cool';
+const sentence3 = 'JavaScript is amazing';
+console.log(sentence1);
+console.log(sentence2);
+console.log(sentence3);
