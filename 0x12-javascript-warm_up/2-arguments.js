b1949575ad82a67db341aa94612ecbe934cde74c
blenassefa2 <blenassefa59@yahoo.com>
2022-11-18 05:58:43 +0300

    javascript warming up3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const { argv } = require('process');
 if (argv.length < 3) {
   console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
