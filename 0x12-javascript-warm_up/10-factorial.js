572df2b54fcdd50cebe483386bc25d1f2c915e27
blenassefa2 <blenassefa59@yahoo.com>
2022-11-19 23:26:40 +0300

    javascript warming up10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+function factorial (number) {
+  if (number == 0 || number == 1) {
+    return 1
+  }
+  return number * factorial(number - 1);
+}
+const n = Number(argv[2]);
+if (!n) {
+  console.log(1);
+} else {
+  console.log(factorial(n));
+}
